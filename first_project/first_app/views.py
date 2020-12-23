76bee6ae40d45c97d5938f64d29ca7f50dbbb58c
refriansyari <refriansyari@gmail.com>
2020-12-23 07:40:15 +0700

    create TEMPLATE_DIR

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dictionary = {'insert_me':"Now I am coming from first_app/index.html!"}
+    return render(request,'first_app/index.html',context=my_dictionary)
