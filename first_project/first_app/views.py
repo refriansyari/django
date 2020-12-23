70c1a95c4bd24a40498b1a784efa20f7cb2a55be
refriansyari <refriansyari@gmail.com>
2020-12-23 20:14:43 +0700

    Display data drom database using Model, Template, Views Paradigm

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,Webpage,AccessRecord
 # Create your views here.
 
 def index(request):
-    my_dictionary = {'insert_me':"Now I am coming from first_app/index.html!"}
-    return render(request,'first_app/index.html',context=my_dictionary)
+    webpages_list = AccessRecord.objects.order_by('date')
+    date_dictionary = {'access_records':webpages_list}
+    return render(request,'first_app/index.html',context=date_dictionary)
